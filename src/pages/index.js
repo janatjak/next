61bb64df42b68b6d3f68f4425ae42a6c2389f375
Jakub Janata <jakubjanata@gmail.com>
2021-05-27 19:08:53 +0200

    all serverless functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
+const apis = ['go', 'node', 'py', 'rb', 'ts'];
+
 const Index = () => {
-    return <div>Init</div>;
+    return <div>
+        <ul>
+            {apis.map(api => <li key={api}><a href={`/api/${api}`} target="_blank">{api}</a></li>)}
+        </ul>
+    </div>;
 }
 
 export default Index;
